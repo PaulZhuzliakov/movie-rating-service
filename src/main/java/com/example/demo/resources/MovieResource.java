bd27ba5c18311ca11f2a9e58452ce2b91ffb885d
PaulZhuzliakov <paulparatrooper@gmail.com>
2021-11-17 07:49:21 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/resources/MovieResource.java
@@ -0,0 +1,16 @@
+package com.example.demo.resources;
+
+import com.example.demo.models.Rating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class MovieResource {
+
+    @RequestMapping("{movieId}")
+    public Rating getMovieInfo(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, "Test name");
+    }
+}
