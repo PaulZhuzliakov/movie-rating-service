32b583f54b819335845853e7bab481d0a7ee9b2a
PaulZhuzliakov <paulparatrooper@gmail.com>
2021-11-19 23:49:12 +0300

    wrap list in object

--- src/main/java/com/example/demo/resources/MovieResource.java
+++ src/main/java/com/example/demo/resources/MovieResource.java
@@ -1,9 +1,13 @@
 package com.example.demo.resources;
 
 import com.example.demo.models.Rating;
+import com.example.demo.models.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -12,5 +16,16 @@ public class MovieResource {
     @RequestMapping("{movieId}")
     public Rating getMovieInfo(@PathVariable("movieId") String movieId) {
         return new Rating(movieId, 5);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("5586", 1),
+                new Rating("5813", 4)
+        );
+        UserRating userRating = new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
